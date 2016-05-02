0e44a25743f527c34957b82e3642eaaee0d8440a
Matteo Capucci <mattecapu@live.it>
2016-05-02 09:22:15 +0200

    cramer linear system solver

---
+++ cramer.m
@@ -0,0 +1,22 @@
+%
+% Risolve un sistema quadrato col metodo di Cramer™
+%
+% INPUT:
+%   A:
+%       Matrice incompleta del sistema, dev'essere regolare
+%   b:
+%       Vettore colonna dei termini noti
+% OUTPUT:
+%   x:
+%       Vettore colonna soluzione del sistema
+%
+
+function [ x ] = cramer(A, b)
+
+    if abs(det(A)) <= eps
+        error('A dev''essere regolare');
+    end
+
+    x = A^-1 * b;
+
+end
