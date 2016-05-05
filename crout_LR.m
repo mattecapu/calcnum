e91ef14765f4a71b64d5a77c8a3cb2cf033749f8
Matteo Capucci <mattecapu@live.it>
2016-05-05 18:32:43 +0200

    Crout LR decomposition

---
+++ crout_LR.m
@@ -0,0 +1,36 @@
+%
+% Decompone la matrice A in due matrici triangolari
+% L (superiore) ed R (inferiroe) usando il metodo di Crout™
+%
+% INPUT
+%	A:
+%		Matrice da decomporre
+%
+% OUTPUT
+%	L,R:
+%		Matrici triangolari tali che L*R = A
+%
+
+function [ L, R ] = crout_LR(A)
+
+	% dimensione di A
+	n = size(A);
+
+	% verifichiamo che A sia definita positiva
+	if ~defpos(A)
+		error('A non è definita positiva');
+	end
+
+	% inizializziamo le matrici
+	L = eye(n);
+	R = zeros(n);
+
+	for i = 1 : n
+		% pseudo-cicli su j e k
+		j = 1 : (i - 1);
+			k = i : n;
+				R(i, k) = A(i, k) - L(i, j) * R(j, k);
+			k = (i + 1) : n;
+				L(k, i) = (A(k, i) - L(k, j) * R(j, i)) / R(i, i);
+	end
+end
