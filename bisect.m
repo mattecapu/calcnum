85ccbeb1e05213d98dcab555ef7f819464108bd2
Matteo Capucci <mattecapu@live.it>
2016-05-05 18:47:59 +0200

    added comments and improved code

--- bisect.m
+++ bisect.m
@@ -1,38 +1,47 @@
+%
+% Trova uno zero di func in [x0, x1] con il metodo di bisezione
+%
+%
+function [ x, it, converged ] = bisect(func, x0, x1, tol = eps, MAX_ITERATIONS = 10^5)
 
-function [ x, it  ] = bisect(func, a, b, tol, MAX_ITERATIONS = 10^9)
-    it = 0;
+    func_x0 = feval(func, x0);
+    func_x1 = feval(func, x1);
 
-    x = [];
-
-    fa = feval(func, a);
-    fb = feval(func, b);
-
-    if fa*fb >= 0
+    if func_x0 * func_x1 >= 0
         error('Il segno di func() dev''essere discorde agli estremi');
     end
 
-    while abs(b - a) >= tol
+    % stima conservativa sul valore di func(x)
+    func_x = Inf;
 
-        x = a + (b - a) / 2;
-        fx = feval(func, x);
+    it = 0;
+    converged = true;
+
+    while abs(x1 - x0) > tol
+
+        % x è il p.to medio di x0x1
+        x = x0 + (x1 - x0) / 2;
+
+        % valutiamo func(x)
+        func_x = feval(func, x);
 
         % se ho trovato lo zero, esco
-        if abs(fx) < tol
-            %fprintf('Arresto per f(x) = %e\n', fx);
-            return;
+        if abs(func_x) < eps
+            break;
         end
 
         % altrimenti aggiorno uno degli estremi
-        if fa*fx < 0
-            b = x;
+        if func_x0 * func_x < eps
+            x1 = x;
         else
-            a = x;
-            fa = fx;
+            x0 = x;
+            func_x0 = func_x;
         end
 
         if ++it >= MAX_ITERATIONS
-            error('Raggiunto il numero massimo di iterazioni')
+            disp('Numero massimo di iterazioni raggiunto');
+            converged = false;
+            break;
         end
-
     end
 end
