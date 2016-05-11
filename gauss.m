59750e5a6e6474b488c14bcd6db4fc008aa120f8
Matteo Capucci <mattecapu@live.it>
2016-05-11 15:47:46 +0200

    comments!

--- gauss.m
+++ gauss.m
@@ -1,3 +1,17 @@
+%
+% Risolve un sistema lineare usando il metodo di eliminazione di gauss
+%
+% INPUT
+%   A:
+%       Matrice incompleta del sistema
+%   b:
+%       Vettore colonna dei termini noti
+%
+% OUTPUT
+%   x:
+%       Soluzione del sistema
+%
+
 function [ x ] = gauss(A, b)
     [C, d] = gauss_triang(A, b);
     x = triang_sis(C, d);
